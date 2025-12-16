64b5e3aa5cf8b8a04bbfb9f6bc5bc1b41e98da17
Sharma678 <shakthisharma0146@gmail.com>
2025-12-16 08:53:58 +0530

    Serve API only on backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Data storage (in-memory, can be replaced with database)
 let events = [];
@@ -123,11 +122,6 @@ app.post('/api/events/:id/rsvp', async (req, res) => {
 app.get('/api/events/:id/rsvps', (req, res) => {
   const eventId = req.params.id;
   res.json(rsvps[eventId] || []);
-});
-
-// Serve React app for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
